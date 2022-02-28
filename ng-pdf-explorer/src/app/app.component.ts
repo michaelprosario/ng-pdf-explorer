4bc7d0ebd2e8a39054aeacb7684216caa255b7a3
michaelprosario <michael.p.rosario@gmail.com>
2022-02-28 23:23:27 +0000

    Got find working

--- ng-pdf-explorer/src/app/app.component.ts
+++ ng-pdf-explorer/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FindResultMatchesCount, FindState, NgxExtendedPdfViewerService } from 'ngx-extended-pdf-viewer';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-pdf-explorer';
+
+
+  public highlightAll = false;
+  public matchCase = false;
+  public wholeWord = false;
+  public ignoreAccents = false;
+
+  public _searchtext = '';
+  findState: FindState;
+  currentMatchNumber: number | undefined;
+  totalMatches: number | undefined;
+
+  public get searchtext(): string {
+    return this._searchtext;
+  }
+
+  public set searchtext(text: string) {
+     if (this.ngxExtendedPdfViewerService.find(
+              text,
+              { highlightAll: this.highlightAll,
+                matchCase: this.matchCase,
+                wholeWords: this.wholeWord,
+                ignoreAccents: this.ignoreAccents})) {
+      this._searchtext = text;
+    }
+  }
+
+  constructor(private ngxExtendedPdfViewerService: NgxExtendedPdfViewerService) {
+    this.findState = FindState.NOT_FOUND;
+  }
+
+  public findNext(): void {
+    this.ngxExtendedPdfViewerService.findNext();
+  }
+
+  public findPrevious(): void {
+    this.ngxExtendedPdfViewerService.findPrevious();
+  }
+
+  public updateFindState(result: FindState) {
+    this.findState = result;
+  }
+
+  public updateFindMatchesCount(result: FindResultMatchesCount) {
+    this.currentMatchNumber = result.current;
+    this.totalMatches = result.total;
+  }
+
+  public onFind(){
+    alert(this.searchtext);
+  }
+    
 }
--- ng-pdf-explorer/src/app/app.module.ts
+++ ng-pdf-explorer/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxExtendedPdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
